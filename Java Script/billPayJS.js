ecdc2a7150eab54ef97f43d2042c31be333c433a
MOKSHITH Y NANDA <codersy17mc@gmail.com>
2025-11-20 21:50:54 +0530

    Initial commit

---
+++ Java Script/billPayJS.js
@@ -0,0 +1,21 @@
+function goto () {
+    document.getElementById( "paybtn" ).style.borderStyle = "inset";
+    if (document.getElementById("billno").value.length==0||document.getElementById("rupee").value.length==0)
+    {   
+        document.getElementById( "error" ).innerHTML = "Above Fields are blank.";
+        document.getElementById( "error" ).style.color = "red";
+        document.getElementById( "error" ).style.textDecoration = "underline";
+        document.getElementById( "paybtn" ).style.borderStyle = "outset";
+    }
+    else
+    {
+        document.getElementById( "error" ).innerHTML = "";
+        alert( "Bill had paid successfully!!." );
+        window.location.replace( "home.html" );
+    }
+}
+    // Check if reCAPTCHA is solved
+    if (!grecaptcha.getResponse()) {
+        alert('Please complete the reCAPTCHA challenge.');
+        return false;
+    }
